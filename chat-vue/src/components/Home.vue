ad46a4f81ed3447b20e23f58f8fb8afdaf58e777
Your Name <you@example.com>
2020-06-13 22:44:09 +0300

    vue autharization

--- chat-vue/src/components/Home.vue
+++ chat-vue/src/components/Home.vue
@@ -1,19 +1,44 @@
 <template>
-    <div>
-      <h1>Чат на vue js</h1>
-      <button @click="goLogin">Вход</button>
+  <div>
+    <h1>Чат на vue js</h1>
+    <button v-if="!auth" @click="goLogin">Вход</button>
+    <div v-if="auth">
+      <button @click="logout" :title="title">Выход</button>
+      <Room/>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Home",
-        methods: {
-          goLogin(){
-            this.$router.push({name: "login"})
-          }
-        },
-    }
+  import Room from '@/components/Room'
+
+  export default {
+    name: "Home",
+    data() {
+      return {
+        title: 'Удалить токен для входа'
+      }
+    },
+    components: {
+      Room
+    },
+    computed: {
+      auth() {
+        if (sessionStorage.getItem("auth_token")) {
+          return true
+        }
+      }
+    },
+    methods: {
+      goLogin() {
+        this.$router.push({name: "login"})
+      },
+      logout() {
+        sessionStorage.removeItem("auth_token")
+        window.location = '/'
+      }
+    },
+  }
 </script>
 
 <style scoped>
